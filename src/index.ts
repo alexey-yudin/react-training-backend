8e3aee13a38a6a16d930cfcfc9de053f104b5b3b
Alexey Yudin <alexey.y.yudin@gmail.com>
2019-08-08 17:26:56 +0200

    Add images.

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '/public/')));
+app.use(express.static(path.join(__dirname, '../public/')));
 
 app.use(bookRoutes);
 
